71368a18f3cd12c6f718b083a922fbefeb271760
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-10-11 22:56:32 +0300

    Add files via upload

---
+++ 03.Programming-for-QA/02.Programming-Advanced-for-QA/02.UnitTesting-Dictionaries,LambdaАndLINQ-Exercise/UnitTesting-Dictionaries,LambdaАndLINQ-Exercise/TestApp.Tests/MinerTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+public class MinerTests
+{
+    [Test]
+    public void Test_Mine_WithEmptyInput_ShouldReturnEmptyString()
+    {
+        // Arrange
+        string[] input = Array.Empty<string>();
+
+        // Act
+        string actual = Miner.Mine(input);
+
+        // Assert
+        Assert.That(actual, Is.Empty);
+    }
+
+    [Test]
+    public void Test_Mine_WithMixedCaseResources_ShouldBeCaseInsensitive()
+    {
+        // Arrange
+        string[] input = new string[] { "GoLd 1", "GOLD 2", "SilVer 18", "SILver 12", "gold 5" };
+
+        // Act
+        string actual = Miner.Mine(input);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo($"gold -> 8{Environment.NewLine}silver -> 30"));
+    }
+
+    [Test]
+    public void Test_Mine_WithDifferentResources_ShouldReturnResourceCounts()
+    {
+        // Arrange
+        string[] input = new string[] { "GoLd 1", "GOLD 2", "SilVer 18", "SILver 12", "gold 5", "copper 100" };
+
+        // Act
+        string actual = Miner.Mine(input);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo($"gold -> 8{Environment.NewLine}silver -> 30{Environment.NewLine}copper -> 100"));
+    }
+}
